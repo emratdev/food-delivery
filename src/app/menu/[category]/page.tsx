8d9a5586a7596a8b2a6e6b54ff0e8c94e4c2cb81
emrat <mirdjalaloff@mail.ru>
2025-07-19 00:44:36 +0500

    update 1

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -2,7 +2,7 @@ import { pizzas } from "@/data";
 import Image from "next/image";
 import Link from "next/link";
 
-// ✅ props turi — hech qanday Promise yo‘q
+
 type Props = {
   params: {
     category: string;
@@ -19,9 +19,9 @@ export default function CategoryPage({ params }: Props) {
       {filteredItems.map((item) => (
         <Link
           key={item.id}
+          href={`/product/${item.id}`}
           className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 
           flex flex-col justify-between group even:bg-fuchsia-50"
-          href={`/product/${item.id}`}
         >
           {item.img && (
             <div className="relative h-[80%]">
@@ -46,8 +46,7 @@ export default function CategoryPage({ params }: Props) {
   );
 }
 
-// ✅ generateStaticParams — bu to‘g‘ri format
-export async function generateStaticParams() {
+export function generateStaticParams() {
   const categories = ["pizza", "burger", "salad"];
   return categories.map((category) => ({
     category,
