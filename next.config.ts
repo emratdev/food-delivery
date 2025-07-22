5058cd003f8cb4dfd9196925bd5139ad21085d4b
emrat <mirdjalaloff@mail.ru>
2025-07-22 19:11:13 +0500

    change 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_BASE_PATH
+      : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_ASSET_PREFIX
+      : "",
   output: "standalone",
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost:3000",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,17 +1,32 @@
-import { Price } from '@/components/Price';
-import { singleProduct } from '@/data';
-import Image from 'next/image';
-import React from 'react'
+// app/product/[id]/page.tsx
 
-const SingleProducts = () => {
+import { Price } from "@/components/Price";
+import { pizzas } from "@/data";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export async function generateStaticParams() {
+  return pizzas.map((item) => ({
+    id: item.id.toString(),
+  }));
+}
+
+const SingleProduct = ({ params }: { params: { id: string } }) => {
+  const product = pizzas.find((p) => p.id.toString() === params.id);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center ">
       {/* IMAGE CONTAINER */}
-      {singleProduct.img && (
+      {product.img && (
         <div className="relative w-full h-1/2 md:h-[70%] ">
           <Image
-            src={singleProduct.img}
-            alt=""
+            src={product.img}
+            alt={product.title}
             className="object-contain"
             fill
           />
@@ -19,16 +34,18 @@ const SingleProducts = () => {
       )}
       {/* TEXT CONTAINER */}
       <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8 ">
-        <h1 className="text-3xl font-bold uppercase xl:text-4xl">{singleProduct.title}</h1>
-        <p>{singleProduct.desc}</p>
+        <h1 className="text-3xl font-bold uppercase xl:text-4xl">
+          {product.title}
+        </h1>
+        <p>{product.desc}</p>
         <Price
-          price={singleProduct.price}
-          id={singleProduct.id}
-          options={singleProduct.options}
+          price={product.price}
+          id={product.id}
+          options={product.options}
         />
       </div>
     </div>
   );
-}
+};
 
-export default SingleProducts
+export default SingleProduct;
